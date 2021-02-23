1d2c9249deb56043c882cba433358e92ac3f2e05
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2021-02-22 22:32:11 -0300

    Day 01 - DONE!

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+//import { Button } from './components/Button';
+import './styles/global.css';
+import { ExperienceBar } from './components/ExperienceBar';
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
